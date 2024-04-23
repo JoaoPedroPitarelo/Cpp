8114c3ec7c8ce0edba236046324577d58f45bd1e
João Pedro Salmazo Pitarelo <142443774+JoaoPedroPitarelo@users.noreply.github.com>
2024-04-23 20:47:16 -0300

    Trabalho Lista Sequencial 23/04
    Criação do arquivo structs.cpp onde serão definidos as estruturas

---
+++ TrabalhoListaSequencial/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <locale.h>
+
+using namespace std;
+#include "structs.cpp"
+
+
+
+
+
+
+// Protótipos de funções
+void inicioPrograma();
+
+
+
+int main() {
+
+    cidade cidade;
+    cidade.nome = "Barueri";
+    cout << cidade.nome;
+}
